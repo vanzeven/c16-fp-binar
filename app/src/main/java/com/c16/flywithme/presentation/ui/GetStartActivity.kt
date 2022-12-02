7e58eccaf8c2ea50d79ddfa6b9167199e305b06d
Fajar Mukhollis <fmukhollis@gmail.com>
2022-12-02 15:55:57 +0700

    edit layout Login

---
+++ app/src/main/java/com/c16/flywithme/presentation/ui/GetStartActivity.kt
@@ -0,0 +1,12 @@
+package com.c16.flywithme.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.c16.flywithme.R
+
+class GetStartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_start)
+    }
+}
