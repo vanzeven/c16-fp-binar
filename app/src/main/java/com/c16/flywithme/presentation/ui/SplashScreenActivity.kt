9ccf52943f63e431eddcc3ba0b227825bb12502a
Fajar Mukhollis <fmukhollis@gmail.com>
2022-12-02 15:55:57 +0700

    edit layout Login

---
+++ app/src/main/java/com/c16/flywithme/presentation/ui/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.c16.flywithme.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.c16.flywithme.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
