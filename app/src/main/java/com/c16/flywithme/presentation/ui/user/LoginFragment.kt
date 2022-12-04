6000585b052005a6ba7638968e21101de0fd8d56
Faisal Reza <7farema@gmail.com>
2022-12-04 22:38:17 +0700

    basic navigation done

--- app/src/main/java/com/c16/flywithme/presentation/ui/user/LoginFragment.kt
+++ app/src/main/java/com/c16/flywithme/presentation/ui/user/LoginFragment.kt
@@ -1,11 +1,10 @@
 package com.c16.flywithme.presentation.ui.user
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.c16.flywithme.R
 import com.c16.flywithme.databinding.FragmentLoginBinding
