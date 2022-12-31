b0a666fcb0dc6b6711b0f40337daf9a963c7d2a4
Faisal Reza <7farema@gmail.com>
2022-12-31 13:19:17 +0700

    udah success get

--- app/src/main/java/com/c16/flywithme/data/response/UserDetailResponse.kt
+++ app/src/main/java/com/c16/flywithme/data/response/UserDetailResponse.kt
@@ -1,63 +1,13 @@
 package com.c16.flywithme.data.response
 
+
 import com.google.gson.annotations.SerializedName
-import java.sql.Struct
 
 data class UserDetailResponse(
-
-	@SerializedName("status")
-	val status: String,
-
-	@SerializedName("meesage")
-	val meesage: String,
-
-	@SerializedName("data")
-	val data: ArrayList<UserItem>
-) {
-
-	data class UserItem(
-
-		@SerializedName("address")
-		val address: String,
-
-		@SerializedName("createdAt")
-		val createdAt: String,
-
-		@SerializedName("dateOfBirth")
-		val dateOfBirth: String,
-
-		@SerializedName("email")
-		val email: String,
-
-		@SerializedName("firstName")
-		val firstName: String,
-
-		@SerializedName("gender")
-		val gender: String,
-
-		@SerializedName("id")
-		val id: Int,
-
-		@SerializedName("image")
-		val image: Any,
-
-		@SerializedName("lastName")
-		val lastName: String,
-
-		@SerializedName("NIK")
-		val NIK: String,
-
-		@SerializedName("password")
-		val password: String,
-
-		@SerializedName("phoneNumber")
-		val phoneNumber: String,
-
-		@SerializedName("roleId")
-		val roleId: String,
-
-		@SerializedName("updatedAt")
-		val updatedAt: String
-
-	)
-}
+    @SerializedName("data")
+    val `data`: Data? = null,
+    @SerializedName("meesage")
+    val meesage: String? = null,
+    @SerializedName("status")
+    val status: String? = null
+)
